58d88e7c1867e666c0c3ede3befd66df5bedabef
Regele Ardealului <undernocircumstances573@gmail.com>
2017-09-30 21:24:45 +0100

    d

---
+++ Example3.ts
@@ -0,0 +1,26 @@
+﻿// Ensure TypeScript compiler is load for VS versions
+// below 2015.
+// https://www.microsoft.com/en-us/download/details.aspx?id=48739
+// Will need a TypeScript definition file for jQuery
+// jquery.d.ts (see NuGet jQuery.TypeScript.DefinedType)
+$(function ()
+{
+    var output: string = "";
+
+    function DisplayLine(text: string): void
+    {
+        output = output + text + "<br/>";
+    }
+
+    // CLASSES
+    // var tb: TextBuilder;
+    var tb: TextBuilder = new TextBuilder("How");
+
+    tb.AppendText(" Are You");
+    tb.AppendText(" Today");
+    DisplayLine(tb.Result);
+    tb.Undo();
+    DisplayLine(tb.Result);
+
+    document.body.innerHTML = output;
+});  
---
+++ TextBuilder.ts
@@ -0,0 +1,59 @@
+﻿// Classes
+class TextBuilder
+{
+    // By default all attributes and methods are public
+
+    // Attributes
+    private _text: string[];
+
+    // Constructor
+    // Only one constructor allowed
+    // Note: No return type for a constructor
+    constructor(initialText: string)
+    {
+        this._text = [];            // Must create array before using it!!
+        this._text[0] = initialText;
+    }
+
+    // Properties
+    // Get and Set Accessors
+    get Text(): string[]
+    {
+        return this._text;
+    }
+
+    set Text(text: string[])
+    {
+        this._text = text;
+    }
+
+    get Result(): string
+    {
+        return this.ReadText();
+    }
+
+    // Methods
+    // Note: You can't overload method names
+    // each method must have a unique name
+    // regardless of signature!!
+    AppendText(text: string): void
+    {
+        this._text.push(text);  // Append to end
+    }
+
+    Undo(): void
+    {
+        this._text.pop();       // Remove last
+    }
+
+    private ReadText(): string
+    {
+        var output: string = "";
+
+        for (var i in this._text)
+        {
+            output = output + this._text[i];
+        }
+        return output;
+    }
+} 
